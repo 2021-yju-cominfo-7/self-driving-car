6f811d237c6acfcfdd04f582eee5529fa0c9635e
JeongJaeSoon <wjdwotns1006@gmail.com>
2021-10-13 10:20:12 +0900

    웹 스트리밍 테스트 컨트롤러 추가

--- serial_arduino.py
+++ serial_arduino.py
@@ -1,4 +1,5 @@
 import serial
+import time
 
 
 def make_serial_connection():
@@ -9,20 +10,11 @@ def make_serial_connection():
     return ser
 
 
-def write_signal(connection, throttles, direction):
-    value_t = throttles
-    value_d = direction + 90
+def write_signal(connection, order):
+    now = time.localtime()
 
-    if value_d <= 60:
-        value_d = 60
-    elif value_d >= 120:
-        value_d = 120
-
-    text_t = f"0{value_t}" if value_t < 10 else f"{value_t}"
-    text_d = f"0{value_d}" if value_d < 100 else f"{value_d}"
-
-    order = f"Q{text_t}{text_d}".encode("utf-8")
-    connection.write(order)
+    print(f"[%02d:%02d:%02d] {order}" % (now.tm_hour, now.tm_min, now.tm_sec))
+    # connection.write(order)
 
 
 def check_order(throttles, direction):
--- web-streaming/streeming.py
+++ web-streaming/streeming.py
@@ -3,10 +3,12 @@ import cv2
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture("../video/ex3.mp4")
+camera = cv2.VideoCapture(1)
+speed = 0
 
 
 def gen_frames():
+    global speed
     while True:
         success, frame = camera.read()
         if not success:
@@ -16,6 +18,7 @@ def gen_frames():
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        print(speed)
 
 
 @app.route('/video_feed')
@@ -26,7 +29,37 @@ def video_feed():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route('/start')
+def start():
+    global speed
+    speed = 9
+    return ("nothing")
+
+
+@app.route('/stop')
+def stop():
+    global speed
+    speed = 0
+    return ("nothing")
+
+
+@app.route('/speed_up')
+def speed_up():
+    global speed
+    speed += 1
+    speed = 13 if speed > 12 else speed
+    return ("nothing")
+
+
+@app.route('/speed_down')
+def speed_down():
+    global speed
+    speed -= 1
+    speed = 0 if speed < 0 else speed
+    return ("nothing")
 
 
 if (__name__ == '__main__'):
