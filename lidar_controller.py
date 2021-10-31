29cfd817b29eacc701d396cd38c11a6a3dedf34e
JeongJaeSoon <wjdwjotns1006@gmail.com>
2021-10-31 16:38:42 +0900

    Add LiDAR sensor sensing algorithm

--- lidar_controller.py
+++ lidar_controller.py
@@ -6,9 +6,6 @@ from math import pi
 
 RMAX = 32.0
 
-
-fig = plt.figure()
-fig.canvas.set_window_title('YDLidar LIDAR Monitor')
 lidar_polar = plt.subplot(polar=True)
 lidar_polar.autoscale_view(True,True,True)
 lidar_polar.set_rmax(RMAX)
@@ -27,11 +24,10 @@ laser.setlidaropt(ydlidar.LidarPropScanFrequency, 10.0)
 laser.setlidaropt(ydlidar.LidarPropSampleRate, 9)
 laser.setlidaropt(ydlidar.LidarPropSingleChannel, False)
 
-
 scan = ydlidar.LaserScan()
 toDegree = 180 / pi
 
-def animate(num):
+def sensing_lidar():
     lidar_flag = True
     r = laser.doProcessSimple(scan)
     if r:
@@ -50,31 +46,53 @@ def animate(num):
                 if 1 < range <= 30:
                     # FRONT SIDE
                     if abs(tmpAngle) >= 160:
-                        print("FRONT STOP")
+                        # print("FRONT STOP")
                         lidar_flag = False
                     # RIGHT SIDE
                     elif point.angle * -1 * toDegree >= 135:
-                        print("RIGHT STOP")
+                        # print("RIGHT STOP")
                         lidar_flag = False
                     # LEFT SIDE
                     elif point.angle * -1 * toDegree <= -135:
-                        print("LEFT STOP")
+                        # print("LEFT STOP")
                         lidar_flag = False
             else:
                 ran.append(0)
             intensity.append(point.intensity)
         
-        print(lidar_flag)
         lidar_polar.clear()
         lidar_polar.scatter(angle, ran, c=intensity, cmap='hsv', alpha=1)
+
+    return lidar_flag
 
 ret = laser.initialize()
 if ret:
     ret = laser.turnOn()
-    if ret:
-        ani = animation.FuncAnimation(fig, animate, interval=50)
-        plt.show()
+    true_count = 0
+    
+    while True:
+        result = sensing_lidar()
+
+        if result:
+            if true_count < 0:
+                true_count = 0
+            else:
+                true_count += 1
+        else:
+            if true_count <= 0:
+                true_count -= 1
+            else:
+                true_count = 0
+
+        if true_count >= 10:
+            print("start")
+        elif true_count <= -3:
+            print("stop")
+        else:
+            print("wait")
+
     laser.turnOff()
+
 laser.disconnecting()
 plt.close()
 
